f02bd860b12a694126338b31af448dc1905c9021
fhk2022 <121293074+fhk2022@users.noreply.github.com>
2023-06-07 22:38:28 +0530

    Update main.tf

--- prod-server/main.tf
+++ prod-server/main.tf
@@ -15,10 +15,10 @@ resource "aws_instance" "test-server" {
   tags = {
     Name = "prod-server"
   }
-  provisioner "local-exec" {
+  provisioner "remote-exec" {
     command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
-  provisioner "local-exec" {
+  provisioner "remote-exec" {
     command = "ansible-playbook /var/lib/jenkins/workspace/HealthCare-Project/prod-server/kuberenetes-playbook.yml"
   } 
 }
