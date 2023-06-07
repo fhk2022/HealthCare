9b47bf80c1e76abc9c327e6379696021cbf55959
root <root@ip-172-31-37-166.ap-south-1.compute.internal>
2023-06-07 14:18:24 +0000

    Adding prod server

---
+++ prod-server/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-0f5ee92e2d63afc18" 
+  instance_type = "t2.micro" 
+  key_name = "Project2"
+  vpc_security_group_ids= ["sg-05f8ed67f2716ebff"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./Project2.pem")
+    host     = self.public_ip
+  }
+  provisioner "local-exec" {
+    command = "sleep 60 && echo 'Instance ready'"
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+    command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/HealthCare-Project/test-server/finance-playbook.yml"
+  } 
+}
