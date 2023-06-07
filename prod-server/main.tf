d4dc1ebf16825f81ba9cce977fc4708b3d15353a
fhk2022 <121293074+fhk2022@users.noreply.github.com>
2023-06-07 23:25:50 +0530

    Update main.tf

--- prod-server/main.tf
+++ prod-server/main.tf
@@ -1,8 +1,11 @@
-resource "aws_instance" "test-server" {
+resource "aws_instance" "prod-server" {
   ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.medium" 
   key_name = "Project2"
   vpc_security_group_ids= ["sg-05f8ed67f2716ebff"]
+  tags = {
+    Name = "prod-server-New" 
+}
   connection {
     type     = "ssh"
     user     = "ubuntu"
@@ -12,10 +15,8 @@ resource "aws_instance" "test-server" {
   provisioner "local-exec" {
     command = "sleep 60 && echo 'Instance ready'"
         }
-  tags = {
-    Name = "prod-server-New" 
-}
-    provisioner "local-exec" {
+
+  provisioner "local-exec" {
     command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
   provisioner "local-exec" {
